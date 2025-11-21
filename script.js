cd485949935409bc3605f14c9f5fcef39f47b08a
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-11-20 19:18:08 -0800

    displays book title

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function book(title, author, pages) {
 
 function addBook(book, library) {
   library.push(book);
+  let container = document.querySelector("#main_container");
+  let book_container = document.createElement("div");
+  book_container.classList.add("container", "book");
+  //create title
+  let title_box = document.createElement("p");
+  title_box.classList.add("title");
+  title_box.textContent = book.title;
+  console.log(title_box.textContent);
+
+  //add book elements to book container
+  book_container.appendChild(title_box);
+  container.appendChild(book_container);
 }
 addBook(new book("title", "yoko", "pages"), myLibrary);
 console.log(myLibrary[0].author);
