ac48c480a28cc39b63a799db85144c9bb0678299
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-11-20 22:44:20 -0800

    finished dialog related interactions

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const myLibrary = [];
+let container = document.querySelector("#main_container");
+let dialog = document.querySelector("dialog");
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
 
 //constructor for books
-function book(title, author) {
+function book(title, author, status) {
   this.title = title;
   this.author = author;
+  this.status = status;
   this.id = crypto.randomUUID;
 }
 //add book function adds a book to the library and returns the library size
@@ -11,9 +16,12 @@ function addBook(book, library) {
   library.push(book);
   return library.length;
 }
+//display books display all library books
 function displayBooks(library) {
+  while (container.lastChild) {
+    container.removeChild(container.firstChild);
+  }
   library.forEach((book) => {
-    let container = document.querySelector("#main_container");
     let book_container = document.createElement("div");
     book_container.classList.add("container", "book", "flex");
 
@@ -21,13 +29,11 @@ function displayBooks(library) {
     let title_box = document.createElement("p");
     title_box.classList.add("title");
     title_box.textContent = book.title;
-    console.log(title_box.textContent);
 
     //create author
     let author_box = document.createElement("p");
     author_box.classList.add("author");
     author_box.textContent = book.author;
-    console.log(author_box.textContent);
 
     //add book elements to book container
     book_container.appendChild(title_box);
@@ -36,7 +42,29 @@ function displayBooks(library) {
   });
 }
 for (let i = 0; i < 10; i++) {
-  addBook(new book("book", "author"), myLibrary);
+  addBook(new book("book", "author", false), myLibrary);
 }
+let openBtn = document.querySelector("#open_dialog");
+openBtn.addEventListener("click", (e) => {
+  dialog.showModal();
+});
+
+let addBtn = dialog.querySelector("#add_book");
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBook(new book(title.value, author.value, false), myLibrary);
+  dialog.close();
+});
+
+dialog.addEventListener("close", (e) => {
+  title.value = "";
+  author.value = "";
+  displayBooks(myLibrary);
+});
+
+let cancelBtn = dialog.querySelector("#cancel");
+cancelBtn.addEventListener("click", (e) => {
+  dialog.close();
+});
 displayBooks(myLibrary);
 console.log(myLibrary[0].author);
