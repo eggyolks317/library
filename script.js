002d5fa8b680eceda35983d615f801bc222f1338
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-11-20 18:57:43 -0800

    created a addBook function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const myLibrary = [];
+
+//constructor for books
+function book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.id = crypto.randomUUID;
+}
+
+function addBook(book, library) {
+  library.push(book);
+}
+addBook(new book("title", "yoko", "pages"), myLibrary);
+console.log(myLibrary[0].author);
